43ad4507d10a49819116eb660ad4e517319a52bb
Mattia <mattia.ceccarelli3@gmail.com>
2019-11-04 15:58:59 +0100

    bipartition :snake:

--- script/networkx_prova.py
+++ script/networkx_prova.py
@@ -3,11 +3,11 @@ import os
 import networkx as nx
 import pylab as plt
 import numpy as np
-path = os.path.join(os.path.dirname('__file__'), '..' ,'data', 'directed_SymptomsNet.gexf')
+path = os.path.join(os.path.dirname('__file__'), '..' ,'data', 'SymptomsNet.gexf')
 
 graph = nx.read_gexf(path)
 
-
+#%%
 len(graph.nodes)
 graph.nodes()
 len(graph.edges)
@@ -24,8 +24,9 @@ A = nx.adjacency_matrix(graph)
 A = nx.to_numpy_matrix(graph)
 print(A)
 e = np.linalg.eigvals(A)
-plt.plot(e)
-np.sum(e)
+plt.plot(e, 'o')
+
+np.sum(e[3:])
 plt.imshow(np.matrix(A))
 nx.degree_centrality(graph)
 
@@ -54,3 +55,38 @@ B.add_edges_from([(1, 'a'), (1, 'b'), (2, 'b'), (2, 'c'), (3, 'c'), (4, 'a')])
 A = nx.to_numpy_matrix(B)
 plt.imshow(A)
 nx.write_gexf(B,"bipartite.gexf")
+
+#%%
+
+class1 = list()
+class2 = list()
+connectivity = dict(graph.adj.items())
+
+for node in connectivity:
+  connectivity[node] = list(connectivity[node])
+
+for node in connectivity:
+
+  if node not in class1 and node not in class2:
+    class1.append(node)
+    class2.extend(connectivity[node])
+
+from collections import Counter
+
+count = Counter(class2)
+
+
+len(class1)
+len(count)
+
+len(count) + len(class1)
+
+rep = 0
+for i, node1 in enumerate(count.keys()):
+  for node2 in count.keys()[i:]:
+    if node1 != node2:
+      if node2 in connectivity[node1]:
+        rep += 1.
+
+
+#%%
