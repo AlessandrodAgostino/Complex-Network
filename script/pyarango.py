09184545929b98bf18efc8b7f384339cc13049d4
riccardo.scheda <riccardo.scheda@gmail.com>
2019-11-14 12:14:09 +0100

    added file pyarango.py in script

---
+++ script/pyarango.py
@@ -0,0 +1,25 @@
+from pyArango.connection import *
+
+##your pswd
+con = Connection(username="root", password="Lalamomo-12")
+
+#load your database
+db = con["symptoms"]
+
+#load your collection
+nodes = db.collections["nodes"]
+edges = db.collections["edges"]
+
+#node1 è un singolo documento della collezione nodes, quindi se ho capito bene ogni nodo è un documento
+#della collezione nodes
+node1 = nodes["N0000"]
+
+#accede al nome del nodo
+def report(document):
+    print("malattia : %s" %document["name"])
+
+report(node1)
+# aql = "FOR x IN nodes RETURN x._key"
+# queryResult = db.AQLQuery(aql, rawResults=True, batchSize=0)
+# for key in queryResult:
+#     print(key)
