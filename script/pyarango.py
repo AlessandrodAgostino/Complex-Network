bb6ab7be0a1e8aaed2992bb89dff7c06b1848d35
Mattia <mattia.ceccarelli3@gmail.com>
2019-11-20 12:08:22 +0100

    thinking about a class

--- script/pyarango.py
+++ script/pyarango.py
@@ -1,95 +1,148 @@
 from pyArango.connection import *
 import json
 
-# your pswd
-#
+# Some try with a Database Class
+
+class Database():
+
+  def __init__(name, username, password, URL='http://127.0.0.1:8529'):
+    '''
+    instantiate a connection with database name, and an object database self._db
+    '''
+
+    # the connection could also fail.
+    conn     = Connection(URL, username=username, password=password)
+
+    try :
+      self._db = conn[name]
+
+    except:
+      raise ValueError(f'No database called {name} found')
+
+  @property
+  def name(self):
+    return self._db.name
+
+  @property
+  def collections(self)
+    return self._db.collections()
+
+  def apply_query(self, name, aql, result):
+    '''
+    Apply the selected query aql to collection, and store the results in a new graph/collection named result.
+    if "result" already exist, it will be overwritten. If it does not exists, it will be created accordingly.
+
+    Parameters:
+      db     : database pyArango object, created by a connection.
+      name   : string, name of the collection or graph to which the query is applied
+      aql    : string, query to apply to "collection"
+      result : string, name of the object (Graph or Query) in which the results are stored.
+
+    Returns:
+
+    Raise:
+      Error if a collection with name 'name' does not exists
+    '''
+
+    if db.hasCollection(name=name):
+
+      if db.hasCollection(name=result):
+        self._db.collections[result].truncate() # delete every document in the collection
+        self._db.AQLQuery(aql, rawResults=True, batchSize=1)
+
+      else :
+        self._db.createCollection('Collection', name=result)
+        queryResult = self._db.AQLQuery(aql, rawResults=True, batchSize=1)
+
+    else :
+      raise Error(f'no collection named {name} in the database {self.name}')
 
 
-def load_pass(filename):
-  '''
-  load password from a json file formatted like :
-  {
-    "username" : "root"
-    "password" : "1234"
-  }
-  '''
+if __name__ == '__main__':
 
-  with open(filename) as f:
-    doc      = json.load(f)
-    password = doc['password']
-  return password
+  # Some try with pyArango
 
-conn = Connection(arangoURL='http://127.0.0.1:8529', username='root', password='')
+  def load_pass(filename):
+    '''
+    load password from a json file formatted like :
+    {
+      "username" : "root"
+      "password" : "1234"
+    }
+    '''
 
-# load your database
-db = conn['_system']
+    with open(filename) as f:
+      doc      = json.load(f)
+      password = doc['password']
+    return password
 
-# load your collections
-nodes = db.collections['Sym_Deas']
-edges = db.collections['Sym_Deas_edges']
+  conn = Connection(arangoURL='http://127.0.0.1:8529', username='root', password='')
 
-def get_name(doc):
-  '''
-  returns nodes name
+  # load your database
+  db = conn['_system']
 
-  Parameters:
-    doc : Document of arango collections
-  '''
+  db.collections
 
-  return doc['name']
+  # load your collections
+  nodes = db.collections['Sym_Deas']
+  edges = db.collections['Sym_Deas_edges']
 
-def empty_collection(db, name : str):
-  '''
-  Check if the collection exist, and delete every document inside
+  def get_name(doc):
+    '''
+    returns nodes name
 
-  Parameters:
-    db   : database object returned by a connection calls
-    name : string, name of the collection to empty
+    Parameters:
+      doc : Document of arango collections
+    '''
 
-  Raise:
-    if the collection does not exist return Error
-  '''
+    return doc['name']
 
-  if db.hasCollection(name=name):
-    db.collections[name].truncate()
+  def empty_collection(db, name : str):
+    '''
+    Check if the collection exist, and delete every document inside
 
-  else:
-    raise Error('The collection does not exist on this database')
+    Parameters:
+      db   : database object returned by a connection calls
+      name : string, name of the collection to empty
 
+    Raise:
+      if the collection does not exist return Error
+    '''
 
-def apply_query(db, name, aql, result):
-  '''
-  Apply the selected query aql to collection, and store the results in a new graph/collection named result.
-  if "result" already exist, it will be overwritten. If it does not exists, it will be created accordingly.
+    if db.hasCollection(name=name):
+      db.collections[name].truncate()
 
-  Parameters:
-    db     : database pyArango object, created by a connection.
-    name   : string, name of the collection or graph to which the query is applied
-    aql    : string, query to apply to "collection"
-    result : string, name of the object (Graph or Query) in which the results are stored.
-
-  Returns:
-
-  Raise:
-
-  '''
-  pass
+    else:
+      raise Error('The collection does not exist on this database')
 
 
 
-if db.hasCollection(name='Astenia'):
+  def apply_query(db, name, aql, result):
+    '''
+    Apply the selected query aql to collection, and store the results in a new graph/collection named result.
+    if "result" already exist, it will be overwritten. If it does not exists, it will be created accordingly.
 
-  db.collections['Astenia'].truncate() # delete every document in the collection
+    Parameters:
+      db     : database pyArango object, created by a connection.
+      name   : string, name of the collection or graph to which the query is applied
+      aql    : string, query to apply to "collection"
+      result : string, name of the object (Graph or Query) in which the results are stored.
 
-  aql = ("for v, e, p in 1 outbound 'Sym_Deas/N0004' Sym_Deas_edges " +
-             "insert {name : v.name, dist : length(p.edges)} in Astenia")
+    Returns:
 
-  queryResult = db.AQLQuery(aql, rawResults=True, batchSize=1)
+    Raise:
 
-else :
+    '''
 
-  db.createCollection('Collection', name='Astenia')
-  aql = ("for v, e, p in 1 outbound 'Sym_Deas/N0004' Sym_Deas_edges " +
-             "insert {name : v.name, dist : length(p.edges)} in Astenia")
+    if db.hasCollection(name=name):
 
-  queryResult = db.AQLQuery(aql, rawResults=True, batchSize=1)
+      if db.hasCollection(name=result):
+        db.collections[result].truncate() # delete every document in the collection
+        db.AQLQuery(aql, rawResults=True, batchSize=1)
+
+      else :
+        db.createCollection('Collection', name=result)
+        queryResult = db.AQLQuery(aql, rawResults=True, batchSize=1)
+
+    else :
+      raise Error(f'no collection named {name} in the database {db.name}')
