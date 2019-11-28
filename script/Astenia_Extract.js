4c41e6a998fd1680afcbec1a5d8977dc5179c145
AlessandrodAgostino <alessandro.dagostino96@gmail.com>
2019-11-28 10:30:07 +0100

    starting using python-arango

--- script/Astenia_Extract.js
+++ script/Astenia_Extract.js
@@ -1,15 +1,5 @@
-// try {
-//   db.Astenia.drop()
-// } catch(error) {}
-// db._create('Astenia')
-// db._query(`for v, e, p in 1 outbound 'Sym_Deas/N0004' Sym_Deas_edges
-//     insert {name : v.name, dist: length(p.edges)} in Astenia`)
-// Astenia = db._query(`FOR c IN Astenia     RETURN c`)
-// let soMany = 10
-// print(`This is ${soMany} times easier!`)
-
-
-neighbours = []
+const fs = require('fs');
+to 
 var res = db._query(`FOR doc in Sym_Deas RETURN doc`);
 while(res.hasNext()){
   var doc = res.next()
@@ -17,5 +7,6 @@ while(res.hasNext()){
     for v, e, p in 1 outbound '${doc['_id']}' Sym_Deas_edges
       return {name : v.name, dist: length(p.edges), origin_id:'${doc['_id']}', origin_name:'${doc['name']}'}`))
 }
-
-print(neighbours[1])
+print(neighbours[3])
+// let data = JSON.stringify(neighbours);
+// fs.writeFileSync('neighbours.json', data);
