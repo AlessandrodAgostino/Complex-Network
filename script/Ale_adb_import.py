b501c93bcfa76615f60a24863a792fe8548ab5ed
alessandro <alessandr.dagostino2@studio.unibo.it>
2019-12-05 12:54:05 +0100

    json revolution

--- script/Ale_adb_import.py
+++ script/Ale_adb_import.py
@@ -1,46 +1,31 @@
-#Ale 10/11/2019
 import os
 import json
-import math
+from math import log
 import networkx as nx
-import pylab as plt
-import numpy as np
-path = os.path.join(os.path.dirname('__file__'), 'data', 'SymptomsNet.gexf')
-graph = nx.read_gexf(path)
-#%%
-nodes = list(graph.nodes())
-length_nod = round(math.log(len(nodes),10)) + 1
+from networkx.readwrite.json_graph import node_link_data
 
-nodes_dict = [{'name': name, '_key': 'N{}'.format(str(n).zfill(length_nod)), 'id' : name} for n,name in enumerate(nodes)]
+#%% Loading Graph path
+path       = os.path.join(os.path.dirname('__file__'), 'data', 'SymptomsNet.gexf')
+graph      = nx.read_gexf(path)
+nodes_list = list(graph.nodes())
 
-'''
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-This JSON has to be imported is a Collection named 'Sym_Deas'
-arangoimport --file ~/Dropbox/Chimera/Sym_nodes_Ale.json --collection Sym_Deas --create-collection true
+json_graph = node_link_data(graph)
 
-'''
-with open('data/Sym_nodes_Ale.json', 'w') as fout:
-    json.dump(nodes_dict , fout, ensure_ascii=False)
+#Future name for Arango vertex collection
+collection_name = 'Sym_Deas'
 
-#%%
+# Number of digit needed for counting the nodes
+format_len_node = round(log(len(json_graph['nodes']),10)) + 1
+# Adding required '_key' attribute for Arango managing
+for n,node in enumerate(json_graph['nodes']):
+  node['_key'] = 'N{}'.format(str(n).zfill(format_len_node)) #Padding in format N0001
 
-edges = list(graph.edges())
-length_edg = round(math.log(len(edges),10)) + 1
-edges_dict = []
+# Number of digit needed for counting the links
+format_len_link = round(log(len(json_graph['links']),10)) + 1
+# Adding required '_key', '_to', '_from' attribute for Arango managing
+for n,link in enumerate(json_graph['links']):
+  link['_key']  = 'E{}'.format(str(n).zfill(format_len_link))
+  link['_to']   = '{}/N{}'.format(collection_name, str(nodes_list.index(link['target'])).zfill(format_len_node))
+  link['_from'] = '{}/N{}'.format(collection_name, str(nodes_list.index(link['source'])).zfill(format_len_node))
 
-for n,edge in enumerate(edges):
-    edges_dict.append({'_from'  : 'Sym_Deas/N{}'.format(str(nodes.index(edge[0])).zfill(length_nod)),
-                       '_to'    : 'Sym_Deas/N{}'.format(str(nodes.index(edge[1])).zfill(length_nod)),
-                       '_key'   : 'E{}'.format(str(n).zfill(length_edg)),
-                       'source' : 'Sym_Deas/'+str(edge[0]),
-                       'target' : 'Sym_Deas/'+str(edge[1]),
-                       })
-
-with open('data/Sym_edges_Ale.json', 'w') as fout:
-    json.dump(edges_dict , fout, ensure_ascii=False)
-
-'''
-Import this file using:
-
-arangoimport --file ~/Dropbox/Chimera/Sym_edges_Ale.json --collection Sym_Deas_edges --create-collection true --create-collection-type edge --batch-size 1655192
-'''
+# Build a collection. insert_many after check_create_empty_collection with apropriate name
