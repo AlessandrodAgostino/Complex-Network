390f60ba22d6dd1badd2b82fafa8cd2a5582881f
riccardoscheda <riccardo.scheda@studio.unibo.it>
2019-12-17 13:34:22 +0100

    modified multipart.py not finished :cry:

--- neaar/multipart.py
+++ neaar/multipart.py
@@ -1,34 +1,20 @@
 import networkx as nx
 import random
-
-
-
-G = nx.complete_multipartite_graph(1,2,3)
-
-
-
-nx.write_gexf(G,"multipartite.gexf")
-
-
 from neaar import pa_utils as pa
 from networkx import read_gexf as rgexf
 from arango import ArangoClient
 
-host, username, password = pa.load_pass(filename='config.json')
 
-# Initialize the client for ArangoDB. Connect to "_system" database as root user.
+host, username, password = pa.load_pass(filename='Complex-Network/config.json')
+# Initializ the client for ArangoDB. Connect to "_system" database as root user.
 client = ArangoClient(hosts=host)
 db     = client.db('_system', username=username, password=password)
 
 
-arango, n = pa.read_gexf(db, "../data/multipartite.gexf")
-nodes = arango.vertex_collection("nodes")
-cursor = nodes.export()
-nodes = cursor.batch()
-nodes
-
-
 #%%
-nx_graph = rgexf("../data/multipartite.gexf")
-import networkx as nx
-nx.draw(nx_graph)
+nx_graph = rgexf("data/multipartite.gexf")
+nodes = list(nx_graph.nodes(data = True))
+
+for n in nodes:
+    collection_name = "collection_"+str(n[1]["subset"])
+    pa.check_create_empty_collection(db, collection_name)
