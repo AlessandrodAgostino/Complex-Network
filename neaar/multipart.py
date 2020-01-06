3b4b20870e30933aa4c1d0d96e766e80bcfda397
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-01-06 18:08:08 +0100

    fixed multipart, need to insert this in readgexf and insert a parameter for multipartite graphs from gexf

--- neaar/multipart.py
+++ neaar/multipart.py
@@ -31,24 +31,15 @@ for n,data in nx_graph.nodes(data=True):
     nodes_collection = pa.check_create_empty_collection(db, collection_name, truncate = False)
     data['_key'] = unidecode.unidecode(str(data['label']))
     nodes_collection.insert(data)
-    classes[str(data['subset'])] = 'collection_'+str(data['subset'])
-
-for n,ed in enumerate(nx_graph.edges(data=True)):
-    graph['links'][n]['_key']  = 'E'+str(n)
-    graph['links'][n]['_from'] =  classes[str(nodes[int(edges[n][0])][1]['subset'])]+'/'+str(edges[n][0])
-    graph['links'][n]['_to']   =  classes[str(nodes[int(edges[n][1])][1]['subset'])]+'/'+str(edges[n][1])
-
-#
-# for n,link in enumerate(graph['links']):
-#   link['_key']  = f'E{n:{0}{format_len_link}}'
-#   link['_to']   = "collection_"+str(n[1]["subset"]) +'/' + unidecode.unidecode(str(link['target']))
-#   link['_from'] = "collection_"+str(n[1]["subset"]) +'/' + unidecode.unidecode(str(link['source']))
-#
+    classes[data['subset']] = 'collection_'+str(data['subset'])
 
 edges_collection = pa.check_create_empty_collection(db=db,collection_name = "edges", edge=True)
-edges_collection.insert_many(graph["links"])
 
-dict(edges_collection)
-# Directly create the graph and add egde collection
+for n,ed in enumerate(nx_graph.edges(data=True)):
+    ed[2]['_key']  = 'E'+str(n)
+    ed[2]['_from'] =  classes[nx_graph.nodes[ed[0]]['subset']] + '/' + str(ed[0])
+    ed[2]['_to']   =  classes[nx_graph.nodes[ed[1]]['subset']] + '/' + str(ed[1])
+    edges_collection.insert(ed[2])
+
 net = pa.check_create_empty_graph(db=db, graph_name="multipartite")
 net.create_edge_definition("edges", ["collection_0","collection_1","collection_2"],["collection_0","collection_1","collection_2"])
