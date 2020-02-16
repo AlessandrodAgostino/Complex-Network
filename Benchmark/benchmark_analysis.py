8c1fadbd928c93cff5f2dc73b2cdde5592bd6747
Mat092 <mattia.ceccarelli3@gmail.com>
2020-02-16 18:57:00 +0100

    Modified with quadratic scaling

--- Benchmark/benchmark_analysis.py
+++ Benchmark/benchmark_analysis.py
@@ -9,8 +9,8 @@ from sklearn.metrics import r2_score
 sns.set_style(style='darkgrid')
 
 # Import data from csv through pandas
-bench_table = pd.read_csv('csv/4_shortest_path.csv', sep='\t', index_col=0)
-# bench_table = pd.read_csv('csv/traverse_0_2.csv', sep='\t', index_col=0)
+# bench_table = pd.read_csv('csv/4_shortest_path.csv', sep='\t', index_col=0)
+bench_table = pd.read_csv('csv/traverse_0_2.csv', sep='\t', index_col=0)
 
 # Computing mean and std deviation of 'Run Time (s)' grouping the data by 'Nodes Number'
 mean_std_df = bench_table.drop(['Probability'], axis=1).groupby(['Nodes Number']).agg({'Run Time (s)':['mean','std']})
@@ -41,11 +41,11 @@ r2    = r2_score(mean, y_fit)
 fig = plt.figure(figsize=(15, 8))
 plt.plot(node_numbers, mean, label='Collected Times')
 plt.fill_between(node_numbers, mean+std, mean-std, alpha=0.5)
-plt.plot(node_numbers, intercept + slope*np.asarray(node_numbers), 'y')
+# plt.plot(node_numbers, intercept + slope*np.asarray(node_numbers), 'y')
 plt.plot(node_numbers, y_fit ,color='r')
 plt.xlabel('Number of nodes', fontsize=25)
 plt.ylabel('Run Time (s)', fontsize=25)
-plt.title('First 4 shortest path query timing', fontsize=30)
+plt.title('Traverse toward second neighbours', fontsize=30)
 
 ticks_size = 25
 plt.rc('xtick', labelsize=ticks_size)
@@ -62,36 +62,37 @@ plt.rc('ytick', labelsize=ticks_size)
 textstr = '\n'.join(('$y(x) = a + bx + cx^2$',
                     # f'a = {intercept * 1e3:.3f} $ms$',
                     # f'b = {slope * 1e6:.3f} $\\pm$ {std_err * 1e6:.3f} $\\mu s$',
-                    # f'$R^2$ = {r_value**2:.3}, degree = 1',
+                    # f'$R^2$ = {r_value**2:.3}',
                     f'$R^2$ = {r2:.3}'))
 
-plt.text(0, 0.3,
+plt.text(0, 0.4,
          s=textstr,
          fontsize=20,
          verticalalignment='top',
          bbox = dict(boxstyle='square', alpha=0.3));
-# plt.savefig('Benchmark/4_shortest_path_timing_readable.png')
+plt.savefig('traverse_0_2_poly.png')
+
 #%% Exponential fit (BROKEN)
-mean = mean.astype(np.int64)
-
-
-from scipy.optimize import curve_fit
-def func_exp(x, a, b):
-    return  b* np.exp(a* x)
-
-popt, perr = curve_fit(func_exp, node_numbers, mean, maxfev=10000 )
-1x_val = np.linspace(node_numbers[0], node_numbers[-1], 100)
-prev_val = func_exp(x_val, *popt)
-
-fig = plt.figure(figsize=(15, 8))
-plt.plot(node_numbers, mean, label='Collected Times')
-plt.fill_between(node_numbers, mean+std, mean-std, alpha=0.5)
-plt.plot(node_numbers, intercept + slope*np.asarray(node_numbers), 'y')
-
-plt.plot(x_val, prev_val ,color='r')
-plt.xlabel('Number of nodes', fontsize=25)
-plt.ylabel('Run Time (s)', fontsize=25)
-plt.title('First 4 shortest - Exponential Fit', fontsize=30)
+# mean = mean.astype(np.int64)
+#
+#
+# from scipy.optimize import curve_fit
+# def func_exp(x, a, b):
+#     return  b* np.exp(a* x)
+#
+# popt, perr = curve_fit(func_exp, node_numbers, mean, maxfev=10000 )
+# x_val = np.linspace(node_numbers[0], node_numbers[-1], 100)
+# prev_val = func_exp(x_val, *popt)
+#
+# fig = plt.figure(figsize=(15, 8))
+# plt.plot(node_numbers, mean, label='Collected Times')
+# plt.fill_between(node_numbers, mean+std, mean-std, alpha=0.5)
+# plt.plot(node_numbers, intercept + slope*np.asarray(node_numbers), 'y')
+#
+# plt.plot(x_val, prev_val ,color='r')
+# plt.xlabel('Number of nodes', fontsize=25)
+# plt.ylabel('Run Time (s)', fontsize=25)
+# plt.title('First 4 shortest - Exponential Fit', fontsize=30)
 
 # ticks_size = 25
 # plt.rc('xtick', labelsize=ticks_size)
